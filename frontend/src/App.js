bb850e913ed1ed362f98b4b67f383c9df20a6ded
Heet Pande <69085878+heetjpande@users.noreply.github.com>
2022-11-08 01:23:51 +0530

    test app -1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
+import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import Footer from './components/Footer';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Login from './components/Login';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      Home Page
-    </div>
-  );
+	return (
+		<Router>
+			<Header/>
+			<Navbar />
+			
+			<Login />
+			{/* <HomePage /> */}
+			<Footer />
+
+		</Router>
+
+	);
 }
 
 export default App;
