6cc47c244ab3b15c421a2d237d11c247e9308958
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-01-03 12:18:18 +0530

    About US Page is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import HomePage from './Pages/Homepage/HomePage'
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer'
+import AboutUs from './Pages/AboutUs/AboutUs'
+
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -20,6 +22,7 @@ function App() {
 				<Routes>
 					<Route exact path='/' element={<HomePage />} />
 					<Route exact path='/home' element={<HomePage />} />
+					<Route exact path='/about' element={<AboutUs />} />
 				</Routes>
 
 				<Footer/>
