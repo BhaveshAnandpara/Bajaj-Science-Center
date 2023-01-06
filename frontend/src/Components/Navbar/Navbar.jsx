57c68110f14e5f4b28bbf8dd6fa1be78bce5d02f
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-01-06 22:09:08 +0530

    navbar Active logic Done

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import './Navbar.css'
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 export default function Navbar() {
+
+    const [activeItemId, setActiveItemId] = useState('Home')
+
+    function activeItem(element) {
+
+        document.getElementById(activeItemId).className = "navItem"
+        element.className = `${element.className + " active "}`
+        setActiveItemId(element.id)
+
+    }
+
     return (
         <>
             <section className="navbar ">
 
-                <Link className='navItem' to='/Home' >Home</Link>
-                <Link className='navItem' to='/About' >About Us</Link>
-                <Link className='navItem' to='/Admission' >Admission</Link>
-                <Link className='navItem' to='/Infrastructure' >Infrastructure</Link>
-                <Link className='navItem' to='/StudentSection' >Student Section</Link>
-                <Link className='navItem' to='/NoticeBoard' >Notice Board</Link>
-                <Link className='navItem' to='/SocialMedia' >Social Media</Link>
-                <Link className='navItem' to='/Login' >Login</Link>
+                <Link id='Home' className='navItem' onClick={(e) => { activeItem(e.target) }} to='/Home' >Home</Link>
+                <Link id='About' className='navItem' onClick={(e) => { activeItem(e.target) }} to='/About' >About Us</Link>
+                <Link id='Admission' className='navItem' onClick={(e) => { activeItem(e.target) }} to='/Admission' >Admission</Link>
+                <Link id='Infrastructure' className='navItem' onClick={(e) => { activeItem(e.target) }} to='/Infrastructure' >Infrastructure</Link>
+                <Link id='StudentSection' className='navItem' onClick={(e) => { activeItem(e.target) }} to='/StudentSection' >Student Section</Link>
+                <Link id='NoticeBoard' className='navItem' onClick={(e) => { activeItem(e.target) }} to='/NoticeBoard' >Notice Board</Link>
+                <Link id='SocialMedia' className='navItem' onClick={(e) => { activeItem(e.target) }} to='/SocialMedia' >Social Media</Link>
+                <Link id='Login' className='navItem' onClick={(e) => { activeItem(e.target) }} to='/Login' >Login</Link>
 
             </section>
         </>
